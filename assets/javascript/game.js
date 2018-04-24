5beffb1e58d380752e052011c8504c88b675aa65
Brian Witte <brianw0991@gmail.com>
2018-04-23 22:02:46 -0500

    some css, javascript, and html were added

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+
+
+// character selection
+let characters = {
+
+    "Trex" = {
+        name: "Tyrannosaurus Rex",
+        health: 250,
+        attack: 20,
+        imageUrl: "assets/images/Trex.gif",
+        enemyAttackBack: 13
+    },
+
+    "Raptor" = { 
+        name: "Velociraptor",
+        health: 175,
+        attack: 16,
+        imageUrl: "assets/images/Raptor.gif",
+        enemyAttackBack: 14
+    },
+
+    "Pterodactyl" = {
+        name: "Pterodactyl",
+        health: 210,
+        attack: 13,
+        imageUrl: "assets/images/Pterodactyl.gif",
+        enemyAttackBack: 12
+    },
+
+    "Triceratops" = {
+        name: "Triceratops",
+        health: 210,
+        attack: 13,
+        imageUrl: "assets/images/triceratops.gif",
+        enemyAttackBack: 15
+    }
+
+}
+
+
+
+
+
+
